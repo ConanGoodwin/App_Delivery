72e4de8f24e5adf2afa1fc7755f5723c557d8563
Danielle <daniellegazarini@gmail.com>
2023-02-27 18:34:38 -0300

    feat: browser-router

---
+++ front-end/src/Routes.js
@@ -0,0 +1,13 @@
+import { Route, Routes } from 'react-router-dom';
+import Login from './pages/Login';
+import Register from './pages/Register';
+
+export default function Router() {
+  return (
+    <Routes>
+      <Route path="/" element={ <Login /> } />
+      <Route path="/login" element={ <Login /> } />
+      <Route path="/register" element={ <Register /> } />
+    </Routes>
+  );
+}
