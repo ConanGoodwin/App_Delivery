7b9b4767e17c7a24108aaf75e7da2fd1eafb23d9
ConanGoodwin <conan_goodwin@hotmail.com>
2023-02-28 01:25:00 -0300

    style:  erros de lint corrigidos

--- back-end/src/api/controllers/user.controller.js
+++ back-end/src/api/controllers/user.controller.js
@@ -1,16 +1,16 @@
-const UserService = require('../services/user.service')
-const { returnController, returnControllerToken } = require('../utils/returnServicesControllers');
 const md5 = require('md5');
+const UserService = require('../services/user.service');
+const { returnController, returnControllerToken } = require('../utils/returnServicesControllers');
 
-const getAll = async (_req,res) => {
+const getAll = async (_req, res) => {
   const { type, message } = await UserService.getAll();
 
   if (type) return res.status(404).json({ message });
-  const arrayPass = message.map(({password}) => password === md5('$#zebirita#$'));
+  const arrayPass = message.map(({ password }) => password === md5('$#zebirita#$'));
   console.log(arrayPass);
 
   returnController(res, type, message, 200);
-}
+};
 
 const login = async (req, res) => {
   const { email, password } = req.body; 
