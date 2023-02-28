2d66c61dd0eab3c4019506d27de5784e395c5f40
ConanGoodwin <conan_goodwin@hotmail.com>
2023-02-28 19:09:15 -0300

    feat: add midlleware que verifica formato do email e tamanho do password(não pode ser menor que 6)

---
+++ back-end/src/api/middlewares/index.js
@@ -0,0 +1,8 @@
+const errorMiddleware = require('./error.middleware');
+const validateBodyUser = require('./validateBodyUser.middleware');
+
+
+module.exports = {
+  errorMiddleware,
+  validateBodyUser,
+};
--- back-end/src/api/routes/user.route.js
+++ back-end/src/api/routes/user.route.js
@@ -1,5 +1,6 @@
 const { Router } = require('express');
 const UserController = require('../controllers/user.controller');
+const validadeBody = require('../middlewares/validateBodyUser.middleware');
 // const { validateBodyUser } = require('../middlewares');
 // const validateJWT = require('../auth/validateJWT');
 
@@ -8,7 +9,7 @@ const userRouter = Router();
 // userRouter.post('/', validateBodyUser, userController.insert);
 userRouter.get('/', UserController.getAll);
 userRouter.post('/', UserController.create);
-userRouter.post('/login', UserController.login);
+userRouter.post('/login', validadeBody, UserController.login);
 // userRouter.get('/:id', validateJWT, userController.getById);
 
 module.exports = userRouter;
