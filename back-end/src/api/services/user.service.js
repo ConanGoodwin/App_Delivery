1b7a9c23ea84bc7b4436a38cf17b7ad792226af3
ConanGoodwin <conan_goodwin@hotmail.com>
2023-02-27 21:05:49 -0300

    feat: add controller de user com getAll

--- back-end/src/api/services/user.service.js
+++ back-end/src/api/services/user.service.js
@@ -3,7 +3,9 @@ const { User } = require("../../database/models");
 const getAll = async () => {
   const [users] = await User.findAll();
 
-  return users;
+  if (users) return { type: null, message: users }
+
+  return { type: 'NOT_FOUND', message: 'Users not found' };
 };
 
 module.exports = {
