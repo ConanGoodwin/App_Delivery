e8ac6e0d62ce6b20b85c5792378ad576fbed2d5f
ConanGoodwin <conan_goodwin@hotmail.com>
2023-02-28 06:23:21 -0300

    feat: requisição de token via axios

--- front-end/src/pages/Login/index.js
+++ front-end/src/pages/Login/index.js
@@ -1,6 +1,40 @@
+import React, { useState } from 'react';
+import api from '../../services/api';
+
 function Login() {
+  const [user, setUser] = useState('');
+
+  const loginClick = async () => {
+    // api.defaults.headers.authorization = 'teste';
+    api
+      .post('/user/login', {
+        email: 'adm@deliveryapp.com',
+        password: '--adm2@21!!--',
+      })
+      .then((response) => setUser(response.data))
+      .catch((err) => {
+        setUser('');
+        console.error(`ops! ocorreu um erro${err}`);
+      });
+    console.log(user.token);
+  };
+
   return (
-    <div>Login</div>
+    <div>
+      Login
+      <p>
+        Usuário:
+        {' '}
+        {user?.token}
+        {user?.message}
+      </p>
+      <button
+        type="button"
+        onClick={ loginClick }
+      >
+        Default
+      </button>
+    </div>
   );
 }
 
