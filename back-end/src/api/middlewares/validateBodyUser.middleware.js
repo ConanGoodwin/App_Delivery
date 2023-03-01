e1b8dd3117097f9652a6b5d26fec6e2208a8a0c8
danilo isaac <daniloisaac942@gmail.com>
2023-03-01 17:46:09 -0300

    refactor:feat: adicionei novas validações e ajustei mensagens de erro

--- back-end/src/api/middlewares/validateBodyUser.middleware.js
+++ back-end/src/api/middlewares/validateBodyUser.middleware.js
@@ -1,13 +1,19 @@
-const validadeBody = async (req, res, next) => {
-  const { name, email, password } = req.body;
+const validateBody = async (req, res, next) => {
+  const { name, email, password, role } = req.body;
 
-    if (name && name.length < 12) {
+    if (!email || !password || !name || !role) {
       return res.status(400).json({
-        message: '"Name" length must be at least 8 characters long',
+        message: 'Some required fields are missing',
       }); 
     }
 
-  if (!(/^[^ ^@]+@[^ ^@^.]+\.[A-Za-z]{2,3}(\.[A-Za-z^.]{2})?$/i).test(email)) {
+    if (name.length < 12) {
+      return res.status(400).json({
+        message: 'The length of the "Name" must be at least 12 characters long',
+      }); 
+    }
+
+  if (!(/^[a-zA-Z0-9._%+-]+@(gmail|hotmail)\.com$/).test(email)) {
     return res.status(400).json({
       message: '"email" must be a valid email',
     }); 
@@ -15,11 +21,11 @@ const validadeBody = async (req, res, next) => {
 
   if (password.length < 6) {
     return res.status(400).json({
-      message: '"password" length must be at least 6 characters long',
+      message: 'The length of the "Password" must be at least 6 characters long',
     }); 
   }
 
   next();
 };
 
-module.exports = validadeBody;
+module.exports = validateBody;
