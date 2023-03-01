4a0ad8cc25053f921f6451d48fc43452fc43416b
danilo isaac <daniloisaac942@gmail.com>
2023-03-01 18:17:00 -0300

    feat: adicionei as validações nas rotas

--- back-end/src/api/routes/user.route.js
+++ back-end/src/api/routes/user.route.js
@@ -1,6 +1,6 @@
 const { Router } = require('express');
 const UserController = require('../controllers/user.controller');
-const validateBody = require('../middlewares/validateBodyUser.middleware');
+const { validateBody, validateEmail, validateLength } = require('../middlewares');
 // const { validateBodyUser } = require('../middlewares');
 // const validateJWT = require('../auth/validateJWT');
 
@@ -8,8 +8,8 @@ const userRouter = Router();
 
 // userRouter.post('/', validateBodyUser, userController.insert);
 userRouter.get('/', UserController.getAll);
-userRouter.post('/register', validateBody, UserController.create);
-userRouter.post('/login', validateBody, UserController.login);
+userRouter.post('/register', validateBody, validateEmail, validateLength, UserController.create);
+userRouter.post('/login', validateBody, validateEmail, validateLength, UserController.login);
 // userRouter.get('/:id', validateJWT, userController.getById);
 
 module.exports = userRouter;
