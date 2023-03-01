5e6d6c57ced8657830481e1ea1a57f82d0d5a5b9
danilo isaac <daniloisaac942@gmail.com>
2023-03-01 17:47:47 -0300

    refactor: renomiei o middleware e renomiei a rota de register

--- back-end/src/api/routes/user.route.js
+++ back-end/src/api/routes/user.route.js
@@ -1,6 +1,6 @@
 const { Router } = require('express');
 const UserController = require('../controllers/user.controller');
-const validadeBody = require('../middlewares/validateBodyUser.middleware');
+const validateBody = require('../middlewares/validateBodyUser.middleware');
 // const { validateBodyUser } = require('../middlewares');
 // const validateJWT = require('../auth/validateJWT');
 
@@ -8,8 +8,8 @@ const userRouter = Router();
 
 // userRouter.post('/', validateBodyUser, userController.insert);
 userRouter.get('/', UserController.getAll);
-userRouter.post('/', validadeBody, UserController.create);
-userRouter.post('/login', validadeBody, UserController.login);
+userRouter.post('/register', validateBody, UserController.create);
+userRouter.post('/login', validateBody, UserController.login);
 // userRouter.get('/:id', validateJWT, userController.getById);
 
 module.exports = userRouter;
