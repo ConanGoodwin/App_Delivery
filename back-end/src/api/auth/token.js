097ccf394af5d0f23df39701acb756a259fc83ec
ConanGoodwin <conan_goodwin@hotmail.com>
2023-03-02 16:57:02 -0300

    chore: usa a chave do arquivo jwt.evaluation.key para gerar o token

--- back-end/src/api/auth/token.js
+++ back-end/src/api/auth/token.js
@@ -1,7 +1,10 @@
-require('dotenv').config();
+const fs = require('fs')
+const path = require('path');
 const jwt = require('jsonwebtoken');
 
-const secret = process.env.JWT_SECRET || 'seusecretdetoken';
+const FILE_PATH = '../../../';
+
+const secret = fs.readFileSync(path.resolve(__dirname,`${FILE_PATH}/jwt.evaluation.key`),'utf8');
 
 const jwtConfig = { expiresIn: '7d', algorithm: 'HS256' };
 
