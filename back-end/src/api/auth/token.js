299311a9dc787e2af1f1e8c1c0c4011708a176ff
ConanGoodwin <conan_goodwin@hotmail.com>
2023-03-02 20:13:11 -0300

    style: fix lint

--- back-end/src/api/auth/token.js
+++ back-end/src/api/auth/token.js
@@ -1,10 +1,10 @@
-const fs = require('fs')
+const fs = require('fs');
 const path = require('path');
 const jwt = require('jsonwebtoken');
 
 const FILE_PATH = '../../../';
 
-const secret = fs.readFileSync(path.resolve(__dirname,`${FILE_PATH}/jwt.evaluation.key`),'utf8');
+const secret = fs.readFileSync(path.resolve(__dirname, `${FILE_PATH}/jwt.evaluation.key`), 'utf8');
 
 const jwtConfig = { expiresIn: '7d', algorithm: 'HS256' };
 
