8e11ecd3c36a8545bcedaaeafa6efbbf2d935725
ConanGoodwin <conan_goodwin@hotmail.com>
2023-03-03 02:53:10 -0300

    feat: criado rota, http://localhost:3001/user/validate, que valida o token e retorna o tipo de usuario

--- back-end/src/api/routes/user.route.js
+++ back-end/src/api/routes/user.route.js
@@ -1,18 +1,16 @@
 const { Router } = require('express');
+const validateJWT = require('../auth/validateJWT');
 const UserController = require('../controllers/user.controller');
 const {
   validateBodyRegister, validateBodyLogin, validateEmail, validateLength, 
 } = require('../middlewares');
-// const { validateBodyUser } = require('../middlewares');
-// const validateJWT = require('../auth/validateJWT');
 
 const userRouter = Router();
 
-// userRouter.post('/', validateBodyUser, userController.insert);
 userRouter.get('/', UserController.getAll);
+userRouter.get('/validate', validateJWT, UserController.validadeToken);
 userRouter
   .post('/register', validateBodyRegister, validateEmail, validateLength, UserController.create);
 userRouter.post('/login', validateBodyLogin, validateEmail, validateLength, UserController.login);
-// userRouter.get('/:id', validateJWT, userController.getById);
 
 module.exports = userRouter;
