58217156bcbef4948a6e03299dfd9b13b84412c1
ConanGoodwin <conan_goodwin@hotmail.com>
2023-03-04 02:19:45 -0300

    fix: att routes

--- front-end/src/Routes.js
+++ front-end/src/Routes.js
@@ -2,6 +2,7 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 import Checkout from './pages/Checkout';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import Products from './pages/Products';
 
 export default function Router() {
   return (
@@ -9,6 +10,7 @@ export default function Router() {
       <Route path="/" element={ <Navigate to="/login" /> } />
       <Route path="/login" element={ <Login /> } />
       <Route path="/register" element={ <Register /> } />
+      <Route path="/customer/products" element={ <Products /> } />
       <Route path="/customer/products/checkout" element={ <Checkout /> } />
     </Routes>
   );
