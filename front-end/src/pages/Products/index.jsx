a04854f93ce2eb76409d91d581001ee7133b70de
ConanGoodwin <conan_goodwin@hotmail.com>
2023-03-04 02:16:01 -0300

    feat: esqueleto completo da tela de checkout

---
+++ front-end/src/pages/Products/index.jsx
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { requestData } from '../../services/api';
+import ProductCard from '../../components/ProductCard';
+
+function Products() {
+  const [allProducts, setAllProducts] = useState([]);
+  const [reqError, setReqError] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        const products = await requestData('/product');
+        setAllProducts(products);
+      } catch (e) {
+        console.log(e);
+        setReqError(true);
+      }
+    };
+    getProducts();
+  }, []);
+  return (
+    <div>
+      <button
+        type="button"
+        onClick={ () => navigate('/custumer/products/checkout') }
+      >
+        Ver Carrinho
+      </button>
+      {
+        !reqError ? allProducts.map(({ id, name, price, urlImage }) => (
+          <div key={ id }>
+            <ProductCard
+              id={ id }
+              name={ name }
+              price={ price }
+              urlImage={ urlImage }
+            />
+          </div>
+        )) : <p>{ erro }</p>
+      }
+    </div>
+  );
+}
+
+export default Products;
