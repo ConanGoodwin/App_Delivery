70cfe47844fd5a1cd1cbab4a111b59c804ee29a0
ConanGoodwin <conan_goodwin@hotmail.com>
2023-03-05 01:03:33 -0300

    fix: corrigido a rota da tela checkout

--- front-end/src/Routes.js
+++ front-end/src/Routes.js
@@ -11,7 +11,7 @@ export default function Router() {
       <Route exact path="/login" element={ <Login /> } />
       <Route exact path="/register" element={ <Register /> } />
       <Route exact path="/customer/products" element={ <Products /> } />
-      <Route exact path="/custumer/checkout" element={ <Checkout /> } />
+      <Route exact path="/customer/checkout" element={ <Checkout /> } />
     </Routes>
   );
 }
