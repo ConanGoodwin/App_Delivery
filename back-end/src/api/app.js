c5543a9586eea6e7517e3b2bb7077e8d840440ac
danilo isaac <daniloisaac942@gmail.com>
2023-03-06 19:54:14 -0300

    feat: criei rotas sales e salesProducts

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { userRouter, productRouter } = require('./routes');
+const { userRouter, productRouter, salesRouter, salesProductsRouter } = require('./routes');
 
 const app = express();
 app.use(express.json());
@@ -9,6 +9,8 @@ app.use(cors());
 app.get('/coffee', (_req, res) => res.status(418).end());
 app.use('/user', userRouter);
 app.use('/product', productRouter);
+app.use('/sales', salesRouter)
+app.use('/salesProducts', salesProductsRouter)
 app.use('/images', express.static('public/images'));
 
 module.exports = app;
