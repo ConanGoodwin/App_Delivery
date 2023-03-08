05a486471dbda6ffcdbab54b878731716475ae9b
Danielle <daniellegazarini@gmail.com>
2023-03-08 16:20:35 -0300

    feat: order details table

---
+++ front-end/src/constant/orderDetails_dataTestId.js
@@ -0,0 +1,8 @@
+const CO = 'customer_order_details';
+
+export const LABEL_DEL_STATUS = `${CO}__element-order-details-label-delivery-status`;
+export const LABEL_ORDER_DATE = `${CO}__element-order-details-label-order-date`;
+export const LABEL_ORDER_ID = `${CO}__element-order-details-label-order-id`;
+export const LABEL_SELLER_NAME = `${CO}__element-order-details-label-seller-name`;
+export const TOTAL_PRICE = `${CO}__element-order-total-price`;
+export const BTN_CHECK = `${CO}__button-delivery-check`;
