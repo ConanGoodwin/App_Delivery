cbfcdb3298d033c3c0d7b49a60e8cc25d68944df
ConanGoodwin <conan_goodwin@hotmail.com>
2023-03-07 21:59:10 -0300

    fix: data-testid sem dois traços tela de pedidos cliente

--- front-end/src/constant/myOrders_dataTestId.js
+++ front-end/src/constant/myOrders_dataTestId.js
@@ -1,7 +1,7 @@
-export const CUSTOMER_ORDERS_ID = 'customer_orders__element-order-id-';
+export const CUSTOMER_ORDERS_ID = 'customer_orders__element-order-id';
 
-export const CUSTOMER_DELIVERY_STATUS_ID = 'customer_orders__element-order-id-';
+export const CUSTOMER_DELIVERY_STATUS_ID = 'customer_orders__element-order-id';
 
-export const COSTUMER_ORDERS_DATE_ID = 'customer_orders__element-order-date-';
+export const COSTUMER_ORDERS_DATE_ID = 'customer_orders__element-order-date';
 
-export const COSTUMER_ORDERS_CARD_PRICE_ID = 'customer_orders__element-card-price-';
+export const COSTUMER_ORDERS_CARD_PRICE_ID = 'customer_orders__element-card-price';
