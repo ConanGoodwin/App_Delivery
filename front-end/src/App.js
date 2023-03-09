728492e17d2e6e99cce43941b37681c4313a0fd3
ConanGoodwin <conan_goodwin@hotmail.com>
2023-03-09 13:06:59 -0300

    fix: position fixed da barra de navegação estava quebrando requisitos

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,6 +3,7 @@ import { useContext } from 'react';
 import Router from './Routes';
 import NavBar from './components/NavBar';
 import LoginContext from './context/LoginContext';
+// import './components/style.css';
 
 function App() {
   const { userLogin } = useContext(LoginContext);
