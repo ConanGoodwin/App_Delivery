e017eb48fb516eccbd05fb59ec0bad18d9e7d855
ConanGoodwin <conan_goodwin@hotmail.com>
2023-03-09 11:59:10 -0300

    style: add base para estilo da tela produtos e navBar

--- front-end/src/components/ProductCard.jsx
+++ front-end/src/components/ProductCard.jsx
@@ -4,42 +4,53 @@ import PropTypes from 'prop-types';
 function ProductCard({
   id, name, price, urlImage, quantidade, txtChange, handleClickMore, handleClickMinus }) {
   return (
-    <div>
-      <p data-testid={ `customer_products__element-card-title-${id}` }>
+    <section>
+      <p
+        data-testid={ `customer_products__element-card-title-${id}` }
+        className="fontLarger"
+      >
         {name}
       </p>
       <p data-testid={ `customer_products__element-card-price-${id}` }>
+        R$
+        {' '}
         {(price).replace('.', ',')}
       </p>
       <img
+        className="img_product"
         data-testid={ `customer_products__img-card-bg-image-${id}` }
         src={ urlImage }
         alt={ name }
       />
-      <button
-        data-testid={ `customer_products__button-card-rm-item-${id}` }
-        type="button"
-        name={ id }
-        onClick={ handleClickMinus }
-      >
-        -
-      </button>
-      <input
-        type="text"
-        data-testid={ `customer_products__input-card-quantity-${id}` }
-        name={ id }
-        value={ quantidade }
-        onChange={ txtChange }
-      />
-      <button
-        data-testid={ `customer_products__button-card-add-item-${id}` }
-        type="button"
-        name={ id }
-        onClick={ handleClickMore }
-      >
-        +
-      </button>
-    </div>
+      <div className="divBtn">
+        <button
+          className="button"
+          data-testid={ `customer_products__button-card-rm-item-${id}` }
+          type="button"
+          name={ id }
+          onClick={ handleClickMinus }
+        >
+          -
+        </button>
+        <input
+          className="imput_qt_product"
+          type="text"
+          data-testid={ `customer_products__input-card-quantity-${id}` }
+          name={ id }
+          value={ quantidade }
+          onChange={ txtChange }
+        />
+        <button
+          className="button"
+          data-testid={ `customer_products__button-card-add-item-${id}` }
+          type="button"
+          name={ id }
+          onClick={ handleClickMore }
+        >
+          +
+        </button>
+      </div>
+    </section>
   );
 }
 
