63a9b100bf29db5853710647f32360e3c010435c
ConanGoodwin <conan_goodwin@hotmail.com>
2023-03-20 15:00:28 -0300

    chore: transferencia de repositorio

--- db.example.sql
+++ db.example.sql
@@ -68,3 +68,4 @@ INSERT INTO products (id, name, price, url_image) VALUES
 	(9, 'Becks 600ml',8.89, 'http://localhost:3001/images/becks_600ml.jpg'),
 	(10, 'Skol Beats Senses 269ml',3.57, 'http://localhost:3001/images/skol_beats_senses_269ml.jpg'),
 	(11, 'Stella Artois 275ml',3.49, 'http://localhost:3001/images/stella_artois_275ml.jpg');
+-- Fim
