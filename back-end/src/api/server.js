5f230fb6635c503051675ec910b52192d8c4e3ac
ConanGoodwin <conan_goodwin@hotmail.com>
2023-03-22 16:34:25 -0300

    test: teste de listen

--- back-end/src/api/server.js
+++ back-end/src/api/server.js
@@ -1,5 +1,16 @@
-const port = process.env.PORT || 3001;
+const express = require('express');
+const path = require('path')
+// const port = process.env.PORT || 3001;
 const app = require('./app');
 
-app.listen(port);
-console.log(`Api rodando na porta ${port}`);
+// app.listen(port);
+// console.log(`Api rodando na porta ${port}`);
+
+const PORT = process.env.PORT || 3001
+
+app
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
