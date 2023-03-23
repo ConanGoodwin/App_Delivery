3b917b6b9b856aa1fc245f9d22eee3e637fcb62a
ConanGoodwin <conan_goodwin@hotmail.com>
2023-03-23 03:26:23 -0300

    test: teste de listen

--- back-end/src/api/server.js
+++ back-end/src/api/server.js
@@ -13,5 +13,5 @@ app
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .listen(PORT,'0.0.0.0', () => console.log(`Listening on ${ PORT }`))
   // teste
