c565077294ce11b222853f8c6bfad1df59dc8729
ConanGoodwin <conan_goodwin@hotmail.com>
2023-03-23 03:46:47 -0300

    test: teste de listen

--- scripts/prepare-for-test.sh
+++ scripts/prepare-for-test.sh
@@ -6,6 +6,7 @@ export NODE_ENV=test
 export CI=false
 pm2 stop all | grep 'PM2'
 pm2 delete all | grep 'PM2'
+npm run stop
 kill -9 $(lsof -t -i:3000) &> /dev/null & kill -9 $(lsof -t -i:3001) &> /dev/null
 printf "\n"
 
