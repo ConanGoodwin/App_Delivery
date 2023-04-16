98d576ca0100af342e1f95cef403117271467623
ConanGoodwin <conan_goodwin@hotmail.com>
2023-04-16 17:34:52 -0300

    chore: att env de conexão para ficar igual as do railway

--- back-end/src/database/config/config.js
+++ back-end/src/database/config/config.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require("dotenv").config();
 
 const environment = process.env.NODE_ENV || "test";
 
@@ -11,15 +11,15 @@ const suffix = {
 };
 
 const options = {
-  host: process.env.HOSTNAME || process.env.MYSQL_HOST || 'localhost',
-  port: process.env.MYSQL_PORT || '3306',
-  database: process.env.MYSQL_DB_NAME,
-    // `${process.env.MYSQL_DB_NAME || 'delivery-app'}${suffix[environment] || suffix.test}`,
-  username: process.env.MYSQL_USER || 'root',
-  password: process.env.MYSQL_PASSWORD || 'password',
-  dialect: 'mysql',
+  host: process.env.HOSTNAME || process.env.MYSQLHOST || "localhost",
+  port: process.env.MYSQLPORT || "3306",
+  database: process.env.MYSQLDATABASE,
+  // `${process.env.MYSQLDATABASE || 'delivery-app'}${suffix[environment] || suffix.test}`,
+  username: process.env.MYSQLUSER || "root",
+  password: process.env.MYSQLPASSWORD || "password",
+  dialect: "mysql",
   dialectOptions: {
-    timezone: 'Z',
+    timezone: "Z",
   },
   logging: false,
 };
