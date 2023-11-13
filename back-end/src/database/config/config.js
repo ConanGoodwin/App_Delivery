51571ff6896b09fc5fd8320fab3a75d7c324d5dc
ConanGoodwin <conan_goodwin@hotmail.com>
2023-11-13 19:21:55 -0300

    chore: config.js adequado para o banco do planetscale

--- back-end/src/database/config/config.js
+++ back-end/src/database/config/config.js
@@ -10,17 +10,33 @@ const suffix = {
   test: "-test",
 };
 
+// const options = {
+//   // host: process.env.HOSTNAME || process.env.MYSQLHOST || "localhost",
+//   host: process.env.MYSQLHOST || "localhost",
+//   port: process.env.MYSQLPORT || "3306",
+//   database: process.env.MYSQLDATABASE,
+//   // `${process.env.MYSQLDATABASE || 'delivery-app'}${suffix[environment] || suffix.test}`,
+//   username: process.env.MYSQLUSER || "root",
+//   password: process.env.MYSQLPASSWORD || "password",
+//   dialect: "mysql",
+//   dialectOptions: {
+//     timezone: "Z",
+//   },
+//   logging: false,
+// };
+
 const options = {
-  // host: process.env.HOSTNAME || process.env.MYSQLHOST || "localhost",
-  host: process.env.MYSQLHOST || "localhost",
-  port: process.env.MYSQLPORT || "3306",
-  database: process.env.MYSQLDATABASE,
-  // `${process.env.MYSQLDATABASE || 'delivery-app'}${suffix[environment] || suffix.test}`,
-  username: process.env.MYSQLUSER || "root",
-  password: process.env.MYSQLPASSWORD || "password",
+  database: "delivery-app",
+  username: "izjqkeo06fgybl66fyom",
+  host: "aws.connect.psdb.cloud",
+  password: "pscale_pw_5AQSvcd54DuSUNFHxdkfDrauotfbV8bDTE4jKlWOKt1",
   dialect: "mysql",
+  // sslaccept: "strict",
   dialectOptions: {
     timezone: "Z",
+    ssl: {
+      rejectUnauthorized: false
+    }
   },
   logging: false,
 };
